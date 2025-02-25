ab7f596e4357513c6057cf21ad5c36ad1868f886
i <17203@portalcedi.edu.mx>
2025-02-25 08:54:19 -0600

    coso

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
+import { OpenaiService } from '../openai.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router, private OpenaiService: OpenaiService) { }
+  ideaPrompt: string = '';
+  generatedIdea: string = '';
+
+  async generateIdea() {
+    if (this.ideaPrompt.trim() == '') {
+      alert('Por favor ingresa una idea inicial.');
+      return;
+    }
+    this.generatedIdea = await this.OpenaiService.generateIdea(this.ideaPrompt);
+  }
 }
