46aefa1af512debc5f7c5165122e4efd36c323cd
i <17203@portalcedi.edu.mx>
2025-03-06 08:35:14 -0600

    paswor reset

--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -13,46 +13,31 @@ import { AlertController } from '@ionic/angular';
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ForgotPasswordPage implements OnInit {
-  constructor(private alertController: AlertController, private router: Router) { }
+  email: string = '';
+
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() { }
 
-  // Función que se ejecuta al hacer submit del formulario
-  async onSubmit() {
-    const email = (document.getElementById('email') as HTMLInputElement).value;
-
-    if (this.validateEmail(email)) {
-      const alert = await this.alertController.create({
-        header: 'Reset Link Sent',
-        message: 'A password reset link has been sent to your email.',
-        buttons: ['OK']
-      });
-
-      await alert.present();
+  async onResetPassword() {
+    if (this.validateEmail(this.email)) {
+      try {
+        await this.auth.resetPassword(this.email);
+        console.log('Password reset email sent successfully.');
+      } catch (error) {
+        console.error('Error sending password reset email:', error);
+      }
     } else {
-      const alert = await this.alertController.create({
-        header: 'Error',
-        message: 'Please enter a valid email address.',
-        buttons: ['OK']
-      });
-
-      await alert.present();
+      console.error('Invalid email address');
     }
   }
 
-  // Función para validar el formato del correo
   validateEmail(email: string): boolean {
-    const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email);
   }
 
-  // Función para ir a la página de login
-  login() {
+  onLogin() {
     this.router.navigateByUrl("login");
-  }
-
-  // Función para ir a la página de sign-up
-  onSignUp() {
-    this.router.navigateByUrl("sign-up");
   }
 }
